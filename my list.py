c82dff895b7a45f623d72914298c1676741c8c09
Ngaka7 <asheryngaka7@gmail.com>
2025-08-04 18:30:53 +0300

    Add files via upload

---
+++ my list.py
@@ -0,0 +1,24 @@
+# Make an empty list.
+my_list = []
+
+# Append elements
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list. append(40)
+
+#insert 15 at the second position (index 1)
+my_list.insert(1,15)
+
+# Extend with another list
+my_list.extend([50, 60, 70])
+
+#Remove the last part.
+my_list.pop()
+
+# Sort the list ascending order
+my_list.sort()
+
+#Index value and output of 30
+Index_of_30=my_list.index(30)
+print("index_of_30")
